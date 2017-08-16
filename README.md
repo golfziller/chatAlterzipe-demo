661a0e20790d033a5a5fc0bb702359d8460d340f
golfziller <31056291+golfziller@users.noreply.github.com>
2017-08-16 13:12:51 +0700

    frist commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# chatAlterzipe-demo" 
---
+++ app.js
@@ -0,0 +1,66 @@
+var express = require('express');
+
+var app = express();
+var path = require('path');
+var port = 33334;
+
+var server = app.listen(port, function () {
+    console.log('Listening on port: ' + port);
+});
+
+app.use(express.static('public'));
+
+
+
+
+
+var io = require('socket.io').listen(server);
+var allClients = [];
+
+
+
+
+
+app.get('/', function (req, res) {
+
+    res.sendFile('views/index.html', { root: __dirname })
+
+});
+
+
+
+io.on('connection', function (socket) {
+
+    var i = allClients.indexOf(socket);
+
+    if (i == -1) {
+        allClients.push(socket);
+        io.emit('online', '' + allClients.length - 1);
+    }
+
+
+
+    socket.on('chat', function (data) {
+        io.emit('chat', data);
+
+    });
+
+    socket.on('disconnect', function () {
+        console.log('Got disconnect!');
+        var i = allClients.indexOf(socket);
+
+        if (i != -1) {
+
+            allClients.splice(i, 1);
+            io.emit('online', '' + allClients.length - 1);
+        }
+
+
+
+    });
+
+
+});
+
+
+
